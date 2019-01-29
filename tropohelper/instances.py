0939d4be7ed4da117512b0cedd0baffd5da64a33
Michael Gorman <michael.gorman@patientpop.com>
2019-01-28 16:25:46 -0800

    Name RDS instances properly

--- tropohelper/instances.py
+++ tropohelper/instances.py
@@ -1,13 +1,21 @@
-from troposphere import Ref, Base64, Equals, Tags
-from troposphere.autoscaling import AutoScalingGroup, LaunchConfiguration
-from troposphere.ec2 import SecurityGroupRule, SecurityGroup, Instance
-from troposphere.rds import DBParameterGroup, DBSecurityGroup, DBInstance
 import troposphere.elasticache as elasticache
+from troposphere import Base64, Equals, Ref, Tags
+from troposphere.autoscaling import AutoScalingGroup, LaunchConfiguration
+from troposphere.ec2 import Instance, SecurityGroup, SecurityGroupRule
+from troposphere.rds import DBInstance, DBParameterGroup, DBSecurityGroup
 
 
-def create_ec2_instance(stack, name, ami, subnetid, keyname, instance_profile="", instance_type="t1.micro", security_groups=(),
-                 user_data=""):
+def create_ec2_instance(stack,
+                        name,
+                        ami,
+                        subnetid,
+                        keyname,
+                        instance_profile="",
+                        instance_type="t1.micro",
+                        security_groups=(),
+                        user_data=""):
     """Add EC2 Instance Resource."""
+
     return stack.stack.add_resource(
         Instance(
             "{0}".format(name),
@@ -18,12 +26,19 @@ def create_ec2_instance(stack, name, ami, subnetid, keyname, instance_profile=""
             SubnetId=subnetid,
             Tags=Tags(Name=name),
             UserData=Base64(user_data),
-            IamInstanceProfile=instance_profile
-        ))
+            IamInstanceProfile=instance_profile))
 
 
-def create_launch_config(stack, name, ami, security_group, instance_type, profile, block_devices=[], user_data=""):
+def create_launch_config(stack,
+                         name,
+                         ami,
+                         security_group,
+                         instance_type,
+                         profile,
+                         block_devices=[],
+                         user_data=""):
     """Add EC2 LaunchConfiguration Resource."""
+
     return stack.stack.add_resource(
         LaunchConfiguration(
             '{0}{1}LC'.format(stack.env, name.replace('_', '')),
@@ -33,12 +48,17 @@ def create_launch_config(stack, name, ami, security_group, instance_type, profil
             InstanceType=instance_type,
             IamInstanceProfile=profile,
             UserData=Base64(user_data),
-            BlockDeviceMappings=block_devices
-        ))
+            BlockDeviceMappings=block_devices))
 
 
-def create_autoscale_group(stack, name, launch_con, vpc_zones, elbs=[], target_groups=[]):
+def create_autoscale_group(stack,
+                           name,
+                           launch_con,
+                           vpc_zones,
+                           elbs=[],
+                           target_groups=[]):
     """Add EC2 AutoScalingGroup Resource."""
+
     return stack.stack.add_resource(
         AutoScalingGroup(
             '{0}{1}ASG'.format(stack.env, name.replace('_', '')),
@@ -51,26 +71,41 @@ def create_autoscale_group(stack, name, launch_con, vpc_zones, elbs=[], target_g
             LoadBalancerNames=elbs,
             TargetGroupARNs=target_groups,
         ))
+
+
 def create_db_param_group(stack, name, description, family, parameters={}):
     """Create a DB Parameter Group"""
+
     return stack.stack.add_resource(
         DBParameterGroup(
             '{0}DBParamGroup'.format(name),
             Description="{0} Parameter Group".format(description),
             Family=family,
-            Parameters=parameters
-        ))
+            Parameters=parameters))
 
-def create_rds_instance(stack, db_instance_identifier, db_name, db_instance_class, db_username, db_password,
-    db_subnet_group, db_security_groups, vpc_security_groups, db_param_group,
-    allocated_storage="20", engine="MySQL", engine_version="5.7.17",
-    storage_encrypted="True", deletion_policy="Retain", multi_az=False, public=False):
 
+def create_rds_instance(stack,
+                        db_instance_identifier,
+                        db_name,
+                        db_instance_class,
+                        db_username,
+                        db_password,
+                        db_subnet_group,
+                        db_security_groups,
+                        vpc_security_groups,
+                        db_param_group,
+                        allocated_storage="20",
+                        engine="MySQL",
+                        engine_version="5.7.17",
+                        storage_encrypted="True",
+                        deletion_policy="Retain",
+                        multi_az=False,
+                        public=False):
     """Add RDS Instance Resource."""
 
     return stack.stack.add_resource(
         DBInstance(
-            'RDSInstance',
+            '{0}RDSInstance'.format(db_instance_identifier.replace('-', '')),
             DBInstanceIdentifier=db_instance_identifier,
             DBName=db_name,
             DBInstanceClass=db_instance_class,
@@ -86,5 +121,4 @@ def create_rds_instance(stack, db_instance_identifier, db_name, db_instance_clas
             StorageEncrypted=storage_encrypted,
             DeletionPolicy=deletion_policy,
             PubliclyAccessible=public,
-            MultiAZ=multi_az
-        ))
+            MultiAZ=multi_az))
